cae8b010d955e6a77f5c023bac51d3c5f2973cbb
Iroennys <100061537+Iroennys@users.noreply.github.com>
2024-09-23 01:37:20 -0400

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7589637467:AAEq_48MmI__NKt6akMzVXwwa-QTXT9HoYc")
+    API_ID = int(os.environ.get("API_ID", 14681595))
+    API_HASH = os.environ.get("API_HASH", "a86730aab5c59953c424abb4396d32d5")
     
     
